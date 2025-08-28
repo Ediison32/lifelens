dfe47a0109bf68b3bec30a44361b8f483cfd8313
romerodaniel2098 <romero.g.daniel@gmail.com>
2025-08-28 10:03:26 -0500

    Login API added

--- loginadmin.js
+++ loginadmin.js
@@ -1,29 +1,26 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { auth } from "./firebaseConfig.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('loginForm');
-  const errorEl = document.getElementById('error');
 
-  form?.addEventListener('submit', function (e) {
+  const loginForm = document.getElementById("loginForm");
+  const errorMsg = document.getElementById("error");
+
+  loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    errorEl.textContent = '';
 
-    const user = document.getElementById('username').value.trim();
-    const pass = document.getElementById('password').value;
+    const email = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
 
-    const ADMIN_USER = 'belabs@riwi.com';
-    const ADMIN_PASS = 'admin123';
-
-    if (user === ADMIN_USER && pass === ADMIN_PASS) {
-      // guardamos sesión sessionStorage
-      const authData = { user: user, role: 'admin', ts: Date.now() };
-      sessionStorage.setItem('belabs_auth', JSON.stringify(authData));
-
-      // debug opcional
-      console.log('Login OK — redirigiendo a admin.html', authData);
-
-      // redirigimos al panel
-      window.location.assign('admin.html');
-    } else {
-      errorEl.textContent = 'Usuario o contraseña incorrectos';
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log("Usuario autenticado:", userCredential.user);
+      localStorage.setItem('belabs_auth', userCredential.user.uid)
+      // Redirige al dashboard
+      window.location.href = "admin.html";
+    } catch (error) {
+      console.error(error.code, error.message);
+      errorMsg.textContent = "Credenciales inválidas o error en el login.";
     }
   });
 });
